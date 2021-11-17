dba5f527c37e54709424e0966739f7e145304773
Eric Lemanissier <e.lemanissier@physioflow.com>
2021-11-17 11:13:47 +0100

    first draft of emscripten app

---
+++ conanfile.py
@@ -0,0 +1,35 @@
+from conans import ConanFile, CMake
+from conans.client import tools
+import os
+
+
+class PfWebApp(ConanFile):
+    name = "PfWebApp"
+    version = "0.0.1"
+
+    settings = "os", "arch", "build_type"
+
+    def export_sources(self):
+        self.copy("shell_minimal.html")
+        self.copy("makefile")
+        self.copy("main.cpp")
+
+    def source(self):
+        tools.get(url="https://github.com/ocornut/imgui/archive/v1.85.tar.gz",
+            sha256="7ed49d1f4573004fa725a70642aaddd3e06bb57fcfe1c1a49ac6574a3e895a77",
+            destination=".",
+            strip_root=True)
+        tools.get(url="https://github.com/epezent/implot/archive/refs/tags/v0.12.tar.gz",
+            sha256="f8bc3b9b58dbabe3a0c0a2ebb8307d8e850012685332a85be36fcc4d4450be56",
+            destination=".",
+            strip_root=True)
+
+
+    def build(self):
+        self.run("%s all IMGUI_DIR=. IMPLOT_DIR=." % self.user_info_build["make"].make, run_environment=True)
+
+    def package(self):
+        self.copy("*", src="web", dst="web")
+
+    def deploy(self):
+        self.copy("*", src="web", dst="web")
