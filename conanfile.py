c201c63b17fbb3d48926aeca24dedaf1af35dead
Eric Lemanissier <e.lemanissier@physioflow.com>
2021-11-18 13:20:24 +0100

    remove unused bits

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
 from conans import ConanFile, CMake
-from conans.client import tools
 
 
 class PfWebApp(ConanFile):
@@ -19,16 +18,6 @@ class PfWebApp(ConanFile):
         self.copy("*.cpp")
         self.copy("*.h")
         self.copy("CMakeLists.txt")
-
-    def source(self):
-        tools.get(url="https://github.com/ocornut/imgui/archive/v1.85.tar.gz",
-            sha256="7ed49d1f4573004fa725a70642aaddd3e06bb57fcfe1c1a49ac6574a3e895a77",
-            destination=".",
-            strip_root=True)
-        tools.get(url="https://github.com/epezent/implot/archive/refs/tags/v0.12.tar.gz",
-            sha256="f8bc3b9b58dbabe3a0c0a2ebb8307d8e850012685332a85be36fcc4d4450be56",
-            destination=".",
-            strip_root=True)
 
     def _configure_cmake(self):
         cmake = CMake(self)
