0f343b79a007bc8bc21b0b34c90abac937c20f62
Eric Lemanissier <e.lemanissier@physioflow.com>
2021-11-18 13:11:27 +0100

    remove unused files

--- recipes/installer_only/all/test_package/conanfile.py
+++
@@ -1,9 +0,0 @@
-from conans import ConanFile, tools
-
-class TestPackageConan(ConanFile):
-
-    settings = "os", "arch"
-
-    def test(self):
-        if not tools.cross_building(self):
-            self.run("some_tool --version")
